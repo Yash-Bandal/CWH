26824bf0aba1541371c20042996c3d0068c4637c
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2025-01-20 19:00:01 +0530

    Update KeyNotes.js

--- Javascript/KeyNotes.js
+++ Javascript/KeyNotes.js
@@ -2,6 +2,6 @@
 //form array from 1 to n..vimp trick learn
 let n = 10;
 let arr = Array.from(Array(n+1).keys());
-console.log(arr);
+console.log(arr); 
  
 //-------------------------------------  
